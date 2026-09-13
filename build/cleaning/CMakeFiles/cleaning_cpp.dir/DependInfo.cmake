d0ef7dc5b011d45529f631d348892d31da74db82
CDR <1977959293@qq.com>
2026-09-13 18:05:33 +0800

    Add files via upload

---
+++ build/cleaning/CMakeFiles/cleaning_cpp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/main.cpp" "CMakeFiles/cleaning_cpp.dir/src/main.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/main.cpp.o.d"
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/panel.cpp" "CMakeFiles/cleaning_cpp.dir/src/panel.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/panel.cpp.o.d"
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/rules.cpp" "CMakeFiles/cleaning_cpp.dir/src/rules.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/rules.cpp.o.d"
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/sha256.cpp" "CMakeFiles/cleaning_cpp.dir/src/sha256.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/sha256.cpp.o.d"
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/writers.cpp" "CMakeFiles/cleaning_cpp.dir/src/writers.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/writers.cpp.o.d"
+  "/home/violet/Workspace/Code/Competition_code/Math/2026_cumcm/code/cleaning_cpp/src/xlsx.cpp" "CMakeFiles/cleaning_cpp.dir/src/xlsx.cpp.o" "gcc" "CMakeFiles/cleaning_cpp.dir/src/xlsx.cpp.o.d"
+  "" "cleaning_cpp" "gcc" "CMakeFiles/cleaning_cpp.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/cleaning/CMakeFiles/cleaning_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cleaning_cpp.dir/link.d"
+  "CMakeFiles/cleaning_cpp.dir/src/main.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/main.cpp.o.d"
+  "CMakeFiles/cleaning_cpp.dir/src/panel.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/panel.cpp.o.d"
+  "CMakeFiles/cleaning_cpp.dir/src/rules.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/rules.cpp.o.d"
+  "CMakeFiles/cleaning_cpp.dir/src/sha256.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/sha256.cpp.o.d"
+  "CMakeFiles/cleaning_cpp.dir/src/writers.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/writers.cpp.o.d"
+  "CMakeFiles/cleaning_cpp.dir/src/xlsx.cpp.o"
+  "CMakeFiles/cleaning_cpp.dir/src/xlsx.cpp.o.d"
+  "cleaning_cpp"
+  "cleaning_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cleaning_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
